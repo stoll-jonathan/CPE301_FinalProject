ca8fffe601876b64b18c55291621b05d3f22b745
stoll-jonathan <jonathanstoll@unr.edu>
2025-12-10 18:18:04 -0800

    added disable button w/ ISR

--- FinalProject.ino
+++ FinalProject.ino
@@ -1,4 +1,4 @@
-/* TODO: add vent stepper motor control, add fan control, add ISR to handle disabled toggling
+/* TODO: add vent stepper motor control, add fan control
 */
 
 // Jonathan Stoll
@@ -12,6 +12,7 @@
   Water Sensor: analog 15
   LEDs (all PB): red 0, yellow 1, green 2, blue 3
   RTC: SDA and SLC (digital 20 and 21)
+  Disabled Button: digital 2
 */
 
 #include <LiquidCrystal.h>
@@ -20,7 +21,10 @@
 #include "RTClib.h"
 
 // On/off button
-bool DISABLED = false;
+volatile bool DISABLED = false;
+volatile unsigned char* ddr_d  = (unsigned char*) 0x2A;
+volatile unsigned char* port_d = (unsigned char*) 0x2B;
+int disabledButtonPin = 2;
 
 // LCD pins <--> Arduino pins
 const int RS = 13, EN = 12, D4 = 11, D5 = 10, D6 = 9, D7 = 8;
@@ -28,6 +32,8 @@ LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 // DHT11 temp & humidity sensor
 DHT11 dht11(6);
+bool firstReading;
+int TemperatureReading, HumidityReading;
 
 // Real Time Clock
 RTC_DS1307 rtc;
@@ -40,10 +46,6 @@ int redPin = 0;
 int yellowPin = 1;
 int greenPin = 2;
 int bluePin = 3;
-
-// Motors
-unsigned char* ddr_h = (unsigned char*) 0x101;
-unsigned char* port_h = (unsigned char*) 0x102;
 
 // UART
 volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
@@ -59,6 +61,7 @@ volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int*  my_ADC_DATA = (unsigned int*) 0x78;
+int WaterLevelReading;
 
 
 void setup() {
@@ -67,6 +70,12 @@ void setup() {
 
   // setup the ADC
   adc_init();
+
+  // setup the DHT11 and water sensor
+  firstReading = true;
+  TemperatureReading = 0;
+  HumidityReading = 0;
+  WaterLevelReading = 0;
 
   // setup the LCD
   lcd.begin(16, 2); // cols, rows
@@ -77,6 +86,9 @@ void setup() {
   *ddr_b |= (1 << yellowPin);
   *ddr_b |= (1 << greenPin);
   *ddr_b |= (1 << bluePin);
+
+  // setup input pins
+  *ddr_d &= ~(1 << disabledButtonPin);
 
   // set all LEDs to off by default
   setBlue(0);
@@ -91,108 +103,115 @@ void setup() {
   if (!rtc.isrunning()) {
       rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-  timeForNextReading = (rtc.now() + TimeSpan(0, 0, 0, 2)); // 2 second buffer to allow things to load
+
+  // Setup enable/disable ISR
+  attachInterrupt(digitalPinToInterrupt(2), toggleDisabled, FALLING);
 }
 
 void loop() {
+  char state = ' ';
+
+  // for convienience, print state of the system every second
+  static unsigned long lastSerialTime = 0;
+  unsigned long now = millis();
+
+  if (now - lastSerialTime >= 1000) {  // 1000 ms = 1 second
+    lastSerialTime = now;
+    WaterLevelReading = adc_read(15);
+    printStatusToSerial(WaterLevelReading);
+  }
+
+
+  static int dht11Result = 0;
+
   if (DISABLED) {
     setBlue(0);
     setGreen(0);
     setYellow(1);
     setRed(0);
 
-    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("System Disabled ");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
   }
   else {
-    char state = ' ';
+    WaterLevelReading = adc_read(15);
 
-    // Gather data
-    int WaterLevelReading = adc_read(15); // analog pin 15
-    int TemperatureReading = 0;
-    int HumidityReading = 0;
-    int dht11Result = dht11.readTemperatureHumidity(TemperatureReading, HumidityReading); // writes readings to TemperatureReading and HumidityReading and stores return status in dht11Result
-  
-    // Output time and water level to serial monitor for debugging
-    U0putchar('\n');
-    U0printInt(rtc.now().hour());
-    U0putchar(':');
-    U0printInt(rtc.now().minute());
-    U0putchar(':');
-    U0printInt(rtc.now().second());
-    U0putchar('\n');
-    U0print("Next Reading at ");
-    U0printInt(timeForNextReading.hour());
-    U0putchar(':');
-    U0printInt(timeForNextReading.minute());
-    U0putchar(':');
-    U0printInt(timeForNextReading.second());
-    U0putchar('\n');
-    U0print("water level: ");
-    U0printInt(WaterLevelReading);
-    U0putchar('\n');
-
-
-    // Output data to LCD
+    // Check water level
     if (WaterLevelReading < 15) {
+      lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Water is too low");
       lcd.setCursor(0, 1);
-      lcd.print("                ");
-      
-      state = 'E'; // error
-      // set fan off
-    }
-    else if (dht11Result == 0) {
-      if (rtc.now() == timeForNextReading) {
-        U0print("Temp and Humidity Readings Updated\n");
-        timeForNextReading = (rtc.now() + TimeSpan(0, 0, 1, 0)); // 1 minute from now
-        
-        
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Temp: "); lcd.print(TemperatureReading);
-
-        lcd.setCursor(0, 1);
-        lcd.print("Humidity: "); lcd.print(HumidityReading);
-        
-        
-        // if temp good set to idle 'I' and set fan to off
-        // if temp no good set to running 'R' and set fan to on
-      }
+      lcd.print("Water Level: "); lcd.print(WaterLevelReading);
+      state = 'E';
     }
     else {
+      // Read DHT11 if time
+      if (rtc.now() >= timeForNextReading || firstReading) {
+        firstReading = false;
+        
+        int result = dht11.readTemperatureHumidity(TemperatureReading, HumidityReading);
+        if (result == 0) { // success
+            dht11Result = 0;
+            timeForNextReading = rtc.now() + TimeSpan(0, 0, 0, 1); // 1 min from now
+        } 
+        else {
+          dht11Result = 1; // error
+        }
+      }
+
+      // Update LCD
       lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("DHT11 error");
-      
-      state = 'E'; // error
-      // set fan off
+      if (dht11Result != 0) {
+        lcd.setCursor(0, 0);
+        lcd.print("DHT11 error");
+        state = 'E';
+      } 
+      else {
+        lcd.setCursor(0, 0);
+        lcd.print("Temperature: "); lcd.print(TemperatureReading);
+        lcd.setCursor(0, 1);
+        lcd.print("Humidity: "); lcd.print(HumidityReading);
+        state = 'I'; // idle
+      }
     }
+  }
 
-
-    // Set LEDs
-    if (state == 'R') { // running
-      setBlue(1);
-      setGreen(0);
-      setYellow(0);
-      setRed(0);
-    }
-    else if (state == 'I') { // idle
-      setBlue(0);
-      setGreen(1);
-      setYellow(0);
-      setRed(0);
-    }
-    else if (state == 'E') { // error
-      setBlue(0);
-      setGreen(0);
-      setYellow(0);
-      setRed(1);
-    }
+  // Set LEDs
+  if (state == 'R') { // running
+    setBlue(1);
+    setGreen(0);
+    setYellow(0);
+    setRed(0);
+  }
+  else if (state == 'I') { // idle
+    setBlue(0);
+    setGreen(1);
+    setYellow(0);
+    setRed(0);
+  }
+  else if (state == 'E') { // error
+    setBlue(0);
+    setGreen(0);
+    setYellow(0);
+    setRed(1);
   }
 
 }
 
+
+void toggleDisabled() {
+  static unsigned long last = 0;
+  unsigned long now = millis();
+
+  // debouncing: ignore events <200 ms apart
+  if (now - last > 200) {
+    DISABLED = !DISABLED;
+    last = now;
+  }
+}
 
 void setBlue(int on) {
   if (on == 1)
@@ -220,6 +239,23 @@ void setRed(int on) {
     *port_b |= (1 << redPin);
   else
     *port_b &= ~(1 << redPin);
+}
+
+void printStatusToSerial(int WaterLevelReading) {
+
+  U0printInt(rtc.now().hour()); U0putchar(':'); U0printInt(rtc.now().minute()); U0putchar(':'); U0printInt(rtc.now().second());
+  U0print("\nNext Reading at ");
+  U0printInt(timeForNextReading.hour()); U0putchar(':'); U0printInt(timeForNextReading.minute()); U0putchar(':'); U0printInt(timeForNextReading.second());
+
+  U0print("\nwater level: ");
+  U0printInt(WaterLevelReading);
+  U0print("\ntemperature: ");
+  U0printInt(TemperatureReading);
+  U0print("\nhumidity: ");
+  U0printInt(HumidityReading);
+  U0putchar('\n');
+  U0putchar('\n');
+
 }
 
 void U0printInt(int num) {
